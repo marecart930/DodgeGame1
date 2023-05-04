1ed695301011ce12575202edf988251b7bdd6afd
marecart930 <marecart930@SDS-L-991HC63>
2023-05-04 14:54:27 -0400

    Add project files.

---
+++ DodgeGame1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DodgeGame1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 12F);
+            this.timeLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(90, 19);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "timeLabel";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 12F);
+            this.scoreLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.scoreLabel.Location = new System.Drawing.Point(489, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(99, 19);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "scoreLabel";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.timeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Dodge Game";
+       
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ DodgeGame1/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DodgeGame1
+{
+    public partial class Form1 : Form
+    {
+        //Hero variables
+        Rectangle hero = new Rectangle(280, 50, 60, 60);
+        int heroSpeed = 10;
+
+        //Ball variables
+        int ballSpeed = 8;
+        int ballSize = 10;
+
+        //List of balls
+        List<Rectangle> balllist = new List<Rectangle>();
+
+        int score = 0;
+        int time = 500;
+
+        bool leftDown = false;
+        bool rightDown = false;
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        int groundHeight = 50;
+        public Form1()
+        {
+            InitializeComponent();
+            Rectangle ball = new Rectangle(20, 0, ballSize, ballSize);
+            balllist.Add(ball);
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            //update labels
+            timeLabel.Text = $"Time Left: {time}";
+            scoreLabel.Text = $"Score: {score}";
+
+            //draw ground
+            e.Graphics.FillRectangle(whiteBrush, 0, this.Height - groundHeight, this.Width, groundHeight);
+
+            //draw hero
+            e.Graphics.FillRectangle(whiteBrush, hero);
+
+            //draw balls
+            for (int i = 0; i < balllist.Count; i++)
+            {
+                e.Graphics.FillEllipse(whiteBrush, balllist[i]);
+            }
+        }
+
+        private void Form1_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (rightDown == true && hero.X < this.Width - hero.Width)
+            {
+                hero.X += heroSpeed;
+            }
+            if (leftDown == true && hero.X > 0)
+            {
+                hero.X -= heroSpeed;
+            }
+            //move the ball rectangles
+            for (int i = 0; i < balllist.Count; i++)
+            {
+                int y = balllist[i].Y + ballSpeed;
+                balllist[i] = new Rectangle(balllist[i].X, y, ballSize, ballSize);
+            }
+
+            //generate new ball object
+
+            randValue = randGen.Next(0, 100);
+
+            if (randValue <= 10)
+            {
+                randValue = randGen.Next(0, this.Width - ballSize - 10);
+
+                Rectangle ball = new Rectangle(randValue, 0, ballSize, ballSize);
+                balllist.Add(ball);
+            }
+
+            //remove the ball if it hits ground
+
+            for (int i = 0; i < balllist.Count; i++)
+            {
+                if (balllist[i].Y >= this.Height)
+                {
+                    balllist.RemoveAt(i);
+                }
+            }
+
+            //check if ball has hit paddle
+            for (int i = 0; i < balllist.Count; i++)
+            {
+                if (balllist[i].IntersectsWith(hero))
+                {
+                    balllist.RemoveAt(i);
+                    score += 5;
+                }
+            }
+
+            //check the time left
+            time--;
+            if (time == 0)
+            {
+                gameTimer.Stop();
+            }
+
+
+            //redraw the screen
+            Refresh();
+
+        }
+    }
+
+}
+
